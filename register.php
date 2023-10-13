c2ea8578bc4f61193d8051da5191d4f0bf2ca30d
KeithLoZeHui <tp067653@mail.apu.edu.my>
2023-10-13 14:06:25 +0800

    reg

---
+++ register.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Choose Register</title>
+    <style>
+        #button {
+            text-align: center;
+            padding: 100px;
+            font-size: 100px;
+        }
+
+    </style>
+</head>
+<body>
+    <div id="button">
+        <button onclick="SellerReg()">Register as Seller</button>
+        <button onclick="CustomerReg()">Register as Customer</button>
+    
+    </div>
+</body>
+</html>
