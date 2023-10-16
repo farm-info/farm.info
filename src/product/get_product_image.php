13e12cb5f3b89fcfa73f4c354f634816496c759e
meow-d <isaacwenyi@gmail.com>
2023-10-16 18:32:06 +0800

    connect product page to database

---
+++ src/product/get_product_image.php
@@ -0,0 +1,26 @@
+<?php
+include("../../database/conn.php");
+
+if ($_GET['id'] == 0) {
+    $image = file_get_contents("../../images/logo.png");
+    header("Content-type: image/jpeg");
+    echo $image;
+    exit();
+}
+
+$query =
+    "SELECT imageData FROM product_images
+    WHERE imageID = ?
+    LIMIT 1";
+$statement = $con->prepare($query);
+$statement->bind_param("s", $_GET['id']);
+$statement->execute();
+$result = $statement->get_result();
+$image = mysqli_fetch_column($result);
+
+if (empty($image)) {
+    $image = file_get_contents("../../images/logo.png");
+}
+
+header("Content-type: image/jpeg");
+echo $image;
