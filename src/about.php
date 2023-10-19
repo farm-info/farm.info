605c6663e91cff34c732a481783b41232d1c3a73
limjonrae <limjonrae@outlook.com>
2023-10-19 14:08:43 +0800

    Added some content

--- src/about.php
+++ src/about.php
@@ -1,7 +1,54 @@
 <?php $title = 'About';
-include "../includes/top.php"; ?>
+include("../template/top.php"); ?>
 
 <!-- someting something somethingdfhslhfalf -->
 
-<?php include "../includes/bottom.php"; ?>
+<div class="about-section">
+  <h1>About Farm.info</h1>
+  <p>Some text about who we are and what we do.</p>
+</div>
+
+<h2 style="text-align:center">Our Team</h2>
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="/Group Assignment/female.png" alt="Jane" style="width:100%">
+      <div class="container">
+        <h2>Jane Doe</h2>
+        <p class="title">CEO & Founder</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>jane@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="/Group Assignment/male.png" alt="Mike" style="width:100%">
+      <div class="container">
+        <h2>Mike Ross</h2>
+        <p class="title">Art Director</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>mike@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="/Group Assignment/male.png" alt="John" style="width:100%">
+      <div class="container">
+        <h2>John Doe</h2>
+        <p class="title">Designer</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>john@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php include("../template/bottom.php"); ?>
 
