bd7ebeb027cd26c2f2895866a777721468152c0b
meow-d <isaacwenyi@gmail.com>
2023-10-26 10:35:27 +0800

    fix a very very basic issue

--- includes/top.php
+++ includes/top.php
@@ -29,7 +29,7 @@ include dirname(__FILE__) . "/../includes/conn.php";
 
             <?php if ($loggedInAsSeller) { ?>
                 <li><a href="/farm.info/src/account/logout.php">Log out</a></li>
-                <li><a href="/farm.info/src/seller/addprod.php">Add Product</a></li>
+                <li><a href="/farm.info/src/seller/insertprod.php">Add Product</a></li>
             <?php } else if ($loggedIn) { ?>
                     <li><a href="/farm.info/src/account/logout.php">Log out</a></li>
                     <li><a href="/farm.info/src/product/cart.php">Cart</a></li>
@@ -39,7 +39,6 @@ include dirname(__FILE__) . "/../includes/conn.php";
                     <li><a href="/farm.info/src/account/login.php">Login</a></li>
             <?php } ?>
 
-            <li><a href="/farm.info/src/seller/insertprod.php">Add Product</a></li>
         </ul>
     </nav>
     </header>
