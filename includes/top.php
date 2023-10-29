1bfe3b044ac8203649eb114abb63cc4e84628356
meow-d <isaacwenyi@gmail.com>
2023-10-29 21:50:45 +0800

    remove some comments

--- includes/top.php
+++ includes/top.php
@@ -49,13 +49,11 @@ include dirname(__FILE__) . "/../includes/conn.php";
     </nav>
     </header>
 
-    <!-- assigns .active class to the link of current page -->
-    <!-- doing it in php would have been better but i couldn't find an elegant way -->
-    <script>
+    <script async>
         var currentPage = window.location.pathname;
-        // alternate version that only uses the last part of the path
-        // for example: /farm.info/src/index.php -> index.php
-        //var currentPage = window.location.pathname.split('/').pop();
+        if (currentPage.endsWith("/")) {
+            currentPage += "index.php";
+        }
         var linkToCurrentPage = document.querySelector(`[href="${currentPage}"]`);
         linkToCurrentPage.classList.add("class", "active");
     </script>
