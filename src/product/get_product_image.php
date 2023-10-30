6026674b32fe6b3a19b7594525353a650c22bad2
meow-d <isaacwenyi@gmail.com>
2023-10-30 18:35:47 +0800

    make code look nicer (it's subjective tho)

--- src/product/get_product_image.php
+++ src/product/get_product_image.php
@@ -5,23 +5,20 @@ $placeholder_image = file_get_contents("../../images/placeholder.png");
 
 if ($_GET['id'] == 0) {
     $image = $placeholder_image;
-    header("Content-type: image/jpeg");
-    echo $image;
-    exit();
-}
+} else {
+    $query =
+        "SELECT imageData FROM product_images
+        WHERE imageID = ?
+        LIMIT 1";
+    $statement = $con->prepare($query);
+    $statement->bind_param("s", $_GET['id']);
+    $statement->execute();
+    $result = $statement->get_result();
+    $image = mysqli_fetch_column($result);
 
-$query =
-    "SELECT imageData FROM product_images
-    WHERE imageID = ?
-    LIMIT 1";
-$statement = $con->prepare($query);
-$statement->bind_param("s", $_GET['id']);
-$statement->execute();
-$result = $statement->get_result();
-$image = mysqli_fetch_column($result);
-
-if (empty($image)) {
-    $image = $placeholder_image;
+    if (empty($image)) {
+        $image = $placeholder_image;
+    }
 }
 
 header("Content-type: image/jpeg");
