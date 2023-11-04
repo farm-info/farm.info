5f7b57ad533446ebd88898e33957470675a87987
meow-d <isaacwenyi@gmail.com>
2023-11-05 02:14:43 +0800

    consistent case

--- src/index.php
+++ src/index.php
@@ -55,7 +55,7 @@ include "../includes/top.php"; ?>
                 <!-- you know what? fuck consistency. i can't work with echos -->
                 <form action="add_to_cart.php" method="post" style="white-space: nowrap; display: inline-block;">
                     <input type="hidden" name="quantity" id="quantity" value="1">
-                    <input type="hidden" name="purchase-type" value="buyNow">
+                    <input type="hidden" name="purchase-type" value="buy-now">
                     <input type="hidden" name="customerID" value="<?= $_SESSION['customerID'] ?>">
                     <input type="hidden" name="productID" value="<?= $row["productID"] ?>">
 
