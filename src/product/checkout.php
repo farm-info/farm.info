597dced1411d17cda1e0a2cb16017427f4321a0f
meow-d <isaacwenyi@gmail.com>
2023-11-05 00:28:33 +0800

    close the form tag

--- src/product/checkout.php
+++ src/product/checkout.php
@@ -57,50 +57,51 @@ if ($loggedIn) {
             <form method="post" action="#">
                 <label>Quantity of product</label><br>
                 <input type="number" name="quantity of product" required><br><br>
+            </form>
 
-                <h1> Payment </h1>
-                <form method="post" action="#">
-                    <label>Select payment method</label><br>
-                    <select id="payment-method" name="Payment Method" required onchange="updatePaymentMethod()">
-                        <option value="E-wallet">E-wallet</option>
-                        <option value="Credit/Debit Card">Credit/Debit Card</option>
-                        <option value="Bank Transfer">Bank Transfer</option>
+            <h1> Payment </h1>
+            <form method="post" action="#">
+                <label>Select payment method</label><br>
+                <select id="payment-method" name="Payment Method" required onchange="updatePaymentMethod()">
+                    <option value="E-wallet">E-wallet</option>
+                    <option value="Credit/Debit Card">Credit/Debit Card</option>
+                    <option value="Bank Transfer">Bank Transfer</option>
 
-                    </select><br>
-                    <input type="text" name="e-wallet" id="e-wallet" style="display:none" placeholder="E Wallet">
-                    <input type="text" name="credit-card" id="credit-card" style="display:none" placeholder="CC">
-                    <input type="text" name="bank-number" id="bank-number" style="display:none" placeholder="Bank Number">
-                    <br>
-                    <script>
-                        function updatePaymentMethod() {
-                            var paymentMethod = document.getElementById("payment-method").value;
+                </select><br>
+                <input type="text" name="e-wallet" id="e-wallet" style="display:none" placeholder="E Wallet">
+                <input type="text" name="credit-card" id="credit-card" style="display:none" placeholder="CC">
+                <input type="text" name="bank-number" id="bank-number" style="display:none" placeholder="Bank Number">
+                <br>
+                <script>
+                    function updatePaymentMethod() {
+                        var paymentMethod = document.getElementById("payment-method").value;
 
-                            if (paymentMethod == 'E-wallet') {
-                                document.getElementById('e-wallet').style.display = 'block';
-                                document.getElementById('credit-card').style.display = 'none';
-                                document.getElementById('bank-number').style.display = 'none';
-                            }
-                            if (paymentMethod == 'Credit/Debit Card') {
-                                document.getElementById('credit-card').style.display = 'block';
-                                document.getElementById('e-wallet').style.display = 'none';
-                                document.getElementById('bank-number').style.display = 'none';
-                            }
-
-                            if (paymentMethod == 'Bank Transfer') {
-                                document.getElementById('bank-number').style.display = 'block';
-                                document.getElementById('credit-card').style.display = 'none';
-                                document.getElementById('e-wallet').style.display = 'none';
-                            }
+                        if (paymentMethod == 'E-wallet') {
+                            document.getElementById('e-wallet').style.display = 'block';
+                            document.getElementById('credit-card').style.display = 'none';
+                            document.getElementById('bank-number').style.display = 'none';
                         }
-                    </script>
+                        if (paymentMethod == 'Credit/Debit Card') {
+                            document.getElementById('credit-card').style.display = 'block';
+                            document.getElementById('e-wallet').style.display = 'none';
+                            document.getElementById('bank-number').style.display = 'none';
+                        }
 
-                    <label>Enter a voucher or giftcard if available</label><br>
-                    <input type="text" name="Voucher or giftcard"><br><br>
-                    <label>Enter a discount code if available</label><br>
-                    <input type="text" name="Discount Code"><br><br>
+                        if (paymentMethod == 'Bank Transfer') {
+                            document.getElementById('bank-number').style.display = 'block';
+                            document.getElementById('credit-card').style.display = 'none';
+                            document.getElementById('e-wallet').style.display = 'none';
+                        }
+                    }
+                </script>
 
-                    <button><a href="../product/delete.php">Place Order?</a></button>
-                </form>
+                <label>Enter a voucher or giftcard if available</label><br>
+                <input type="text" name="Voucher or giftcard"><br><br>
+                <label>Enter a discount code if available</label><br>
+                <input type="text" name="Discount Code"><br><br>
+
+                <button><a href="../product/delete.php">Place Order?</a></button>
+            </form>
         </section>
     </div>
 </main>
