5b39e8b16029a409cccfe25a4de860df304cb663
meow-d <isaacwenyi@gmail.com>
2023-11-05 17:59:50 +0800

    fix

--- src/product/add_to_cart.php
+++ src/product/add_to_cart.php
@@ -2,11 +2,18 @@
 include '../../includes/session.php';
 include "../../includes/conn.php";
 
+if (!isset($_POST['productID'])) {
+    echo "<script>alert('ProductID not set!');</script>";
+    header("Location: ../product/index.php");
+    exit();
+}
+
+$productID = $_POST['productID'][0];
 $query = "INSERT INTO cart (customerID, productID, quantity)
         VALUES (?, ?, ?)
         ON DUPLICATE KEY UPDATE quantity = quantity + VALUES(quantity)";
 $statement = $con->prepare($query);
-$statement->bind_param("ssi", $_POST['customerID'], $_POST['productID'], $_POST['quantity']);
+$statement->bind_param("ssi", $_POST['customerID'], $productID, $_POST['quantity']);
 $statement->execute();
 
 if ($statement->affected_rows > 0) {
